3b3fbe6cf6286e3d634162d54119257f5c474972
Abdul Hamid Dar <darhameed@gmail.com>
2020-02-13 12:07:32 +0530

    Temporary fix for logger config in travis

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,8 +1,9 @@
 import loggerFactory, { configure } from 'log4js';
 
 if (!process.env.LOGGER_CONFIG) {
-	console.log('Missing LOGGER_CONFIG in environment');
-	process.exit(1);
+	console.log('Missing LOGGER_CONFIG in environment ... setting logger config');
+	process.env.LOGGER_CONFIG = `{"appenders":{"out":{"type":"stdout","layout":{"type":"pattern","pattern":"%[[%d] [%p] %c - %G{correlationId}%] - %m%n"}}},"categories":{"default":{"appenders":["out"],"level":"info"}}}`;
+	configure(JSON.parse(process.env.LOGGER_CONFIG));
 } else {
 	configure(JSON.parse(process.env.LOGGER_CONFIG));
 }
